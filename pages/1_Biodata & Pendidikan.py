5a0039e2c52ace90887ffa9191365b53d21a3368
ivantriyadi <ivantriyadi@gmail.com>
2024-12-21 10:49:56 +0700

    Add files via upload

---
+++ pages/1_Biodata & Pendidikan.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st. write("page 1")
+st.markdown(
+    """
+    didasdsadsad
+    dsadsad
+
+    sdadsadsa
+    dasdsasdad
+    """
+)
+
+st.download_button('test')
