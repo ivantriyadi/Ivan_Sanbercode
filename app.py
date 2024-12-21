048c51f5fda602196ca3c4567c9cecb6851b7b0a
ivantriyadi <ivantriyadi@gmail.com>
2024-12-21 12:21:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st. write("hello world")
-st.markdown(
-    """
-    didasdsadsad
-    dsadsad
-
-    sdadsadsa
-    dasdsasdad
-    """
-)
+st. header("WELCOME")
+st.image("assets/cv_full.jpg")
