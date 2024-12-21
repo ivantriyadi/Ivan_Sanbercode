816ea6234fa2d3274245e970d6ccce937f320742
ivantriyadi <ivantriyadi@gmail.com>
2024-12-21 11:34:01 +0700

    Update 1_Biodata & Pendidikan.py

--- pages/1_Biodata & Pendidikan.py
+++ pages/1_Biodata & Pendidikan.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-st. write("page 1")
-st.markdown(
-    """
-    didasdsadsad
-    dsadsad
+st.title("BIODATA DAN PENDIDIKAN")
+st.header("IVAN TRIYADI")
 
-    sdadsadsa
-    dasdsasdad
-    """
-)
+col_1, col_2 = st.columns([1,2])
 
-st.download_button('test')
+with col_1:
+    st.subheader("Biodata")
+    st.text("Nama:Ivan Triyadi")
+
+    st.subheader("Alamat")
+    st.text("Serua Residence, Depok")
+
+with col_2:
+    st.subheader("Kegiatan")
+    st.text("Fulltime: kantor GE")
+
+    st.subheader("Kontak")
+    st.text("Phone: +628111588698")
+    st.text("Instagram: ivantriyadi")
